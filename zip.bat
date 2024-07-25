698fb992ec083d5ef5ef5ce4bfe38c2a86bad66e
INCPI <omk14p@outlook.com>
2024-07-25 11:09:30 +0530

    v_3.4.5

--- zip.bat
+++ zip.bat
@@ -1,7 +1,14 @@
 @echo off
 
 rem Remove existing zip file
-del /q bin\*.zip
+del /q "bin\*.zip"
+
+rem Check if manifest.json exists
+if not exist manifest.json (
+    echo manifest.json not found!
+    pause
+    exit /b 1
+)
 
 rem Read name and version from manifest.json
 for /f "usebackq tokens=2 delims=:" %%a in (`type manifest.json ^| findstr /C:"\"name\":" /C:"\"version\":"`) do (
@@ -15,12 +22,13 @@ for /f "usebackq tokens=2 delims=:" %%a in (`type manifest.json ^| findstr /C:"\
 )
 
 rem Remove double quotes and leading/trailing spaces from the name and version strings
-set name=%name:"=%
-set version=%version:"=%
-set version=%version:,=%
+set "name=%name:"=%"
+set "version=%version:"=%"
+set "version=%version:,=%"
+
 rem Replace spaces with underscores in the name
-set name=%name: =_%
-set name=%name:,=%
+set "name=%name: =_%"
+set "name=%name:,=%"
 
 rem Echo the filename
 echo %name%-%version%.zip
